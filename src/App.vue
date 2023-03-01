6f9a31b06539857a0dc44124e49d8be0fd711c77
nicolas garces <nicolasgarces0@gmail.com>
2023-03-01 18:59:58 -0500

    second commit

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,53 @@ const styleColor = 'color: blue';
 const arrayColor = [ "Blue", "Red", "green"];
 const activo = true;
 const arrayFrutas = [ "🍍", "🍌", "🍉", "🍏", "🍑"];
+const frutas = [
+  {
+    name: 'mango',
+    price: '$7.00',
+    description: 'un mango'
+  },
+  {
+    name: 'banano',
+    price: '$2.00',
+    description: 'un banano'
+  },
+  {
+    name: 'limon',
+    price: '$5.00',
+    description: 'un limon'
+  },
+]
+const frutas2 = [
+  {
+    name: 'mango',
+    price: '$7.00',
+    description: 'un mango',
+    stock: 10
+  },
+  {
+    name: 'banano',
+    price: '$2.00',
+    description: 'un banano',
+    stock: 20
+  },
+  {
+    name: 'limon',
+    price: '$5.00',
+    description: 'un limon',
+    stock: 20
+  },
+]
+const guitars = {
+  brand: 'gibson',
+  price : 1000,
+  color: 'black'
+}
 </script>
 
 
 <template>
-  <div>
+  
     
   <!-- usando la interpolacion -->
   <h1>Hola {{name.toUpperCase()}}</h1> 
@@ -29,7 +71,31 @@ const arrayFrutas = [ "🍍", "🍌", "🍉", "🍏", "🍑"];
          {{ index }} - {{ frutas }}
        </li>
     </ul>
-  </div>
+
+    <br>
+<!-- ejemplo 2  de for-->
+    <ul>
+        <li v-for=" fruta in frutas" :key="fruta.name">
+        {{ fruta.name }} - {{ fruta.price }} - {{ fruta.description }}
+        </li>
+    </ul>
+    <br>
+<!-- ejemplo 3 de for -->
+    <ul>
+      <li v-for="guitar, key, index in guitars"> 
+      {{index}} - {{ key }} : {{ guitar }}
+      </li>
+    </ul>
+
+    <br>
+
+<!-- ejemplo 4 de for usando el v-if-->
+  <template v-for="fruta in frutas2" :key="fruta.name" >
+    <li v-if="fruta.stock == 20">
+        {{ fruta.name }} - {{ fruta.price }}
+    </li>
+  </template>
+
 </template>
 
 <style>
